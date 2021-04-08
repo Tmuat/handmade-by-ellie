2ac701ebad20831cfba353ae2e4be77216350ec0
Tmuat <Thomas_Muat@hotmail.com>
2021-04-08 14:06:44 +0000

    USERS: Created CustomUser model using AbstractUser

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    username = None
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'),
+                                  max_length=30, blank=False, null=False)
+    last_name = models.CharField(_('last name'),
+                                 max_length=150, blank=False, null=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
