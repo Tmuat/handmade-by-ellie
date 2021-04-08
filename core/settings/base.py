9509e4fb3eaf2d26e172b3a6218d887bf1e37e94
Tmuat <Thomas_Muat@hotmail.com>
2021-04-08 15:33:49 +0000

    SETUP: Added template dirs

--- core/settings/base.py
+++ core/settings/base.py
@@ -174,6 +174,8 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         # https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
         'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
