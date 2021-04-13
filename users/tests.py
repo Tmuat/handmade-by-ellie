784647ec5348f26eab365b06e85650b409811f27
Tmuat <Thomas_Muat@hotmail.com>
2021-04-13 16:13:21 +0000

    TESTING: Adjusted old tests to pytest format

--- users/tests.py
+++ users/tests.py
@@ -1,60 +1,48 @@
 import pytest
 
-from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
 pytestmark = pytest.mark.django_db
 
 
-def test_hello_world():
-    assert "hello_world" == "hello_world"
+def test_create_user():
+    User = get_user_model()
+    user = User.objects.create_user(
+        email="normal@user.com", first_name="test", last_name="user", password="foo"
+    )
+    assert user.email == "normal@user.com"
+    assert user.first_name == "test"
+    assert user.last_name == "user"
+    assert user.is_active is True
+    assert user.is_staff is False
+    assert user.is_superuser is False
+    assert str(user) == "normal@user.com"
+    # username is None for the AbstractUser option
+    assert user.username is None
+    with pytest.raises(TypeError):
+        User.objects.create_user()
+    with pytest.raises(TypeError):
+        User.objects.create_user(email="")
+    with pytest.raises(ValueError):
+        User.objects.create_user(email="", password="foo")
 
 
-class UsersManagersTests(TestCase):
-    """
-    This code is taken from the following tutorial:
-    https://testdriven.io/blog/django-custom-user-model/
-    """
-
-    def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(
-            email="normal@user.com", first_name="test", last_name="user", password="foo"
+def test_create_superuser():
+    User = get_user_model()
+    admin_user = User.objects.create_superuser("super@user.com", "foo")
+    assert admin_user.email == "super@user.com"
+    assert admin_user.is_active is True
+    assert admin_user.is_staff is True
+    assert admin_user.is_superuser is True
+    assert str(admin_user) == "super@user.com"
+    # username is None for the AbstractUser option
+    assert admin_user.username is None
+    with pytest.raises(ValueError):
+        User.objects.create_superuser(
+            email="super@user.com", password="foo", is_superuser=False
         )
-        self.assertEqual(user.email, "normal@user.com")
-        self.assertEqual(user.first_name, "test")
-        self.assertEqual(user.last_name, "user")
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
-        try:
-            # username is None for the AbstractUser option
-            # username does not exist for the AbstractBaseUser option
-            self.assertIsNone(user.username)
-        except AttributeError:
-            pass
-        with self.assertRaises(TypeError):
-            User.objects.create_user()
-        with self.assertRaises(TypeError):
-            User.objects.create_user(email="")
-        with self.assertRaises(ValueError):
-            User.objects.create_user(email="", password="foo")
-
-    def test_create_superuser(self):
-        User = get_user_model()
-        admin_user = User.objects.create_superuser("super@user.com", "foo")
-        self.assertEqual(admin_user.email, "super@user.com")
-        self.assertTrue(admin_user.is_active)
-        self.assertTrue(admin_user.is_staff)
-        self.assertTrue(admin_user.is_superuser)
-        try:
-            # username is None for the AbstractUser option
-            # username does not exist for the AbstractBaseUser option
-            self.assertIsNone(admin_user.username)
-        except AttributeError:
-            pass
-        with self.assertRaises(ValueError):
-            User.objects.create_superuser(
-                email="super@user.com", password="foo", is_superuser=False
-            )
+    with pytest.raises(ValueError):
+        User.objects.create_superuser(
+            email="super@user.com", password="foo", is_staff=False
+        )
