5a8a671d2f1d287bca1a79e64179ea1a89d63f79
Tmuat <Thomas_Muat@hotmail.com>
2021-04-14 11:22:09 +0000

    CORE: Adjusted formatting

--- core/settings/base.py
+++ core/settings/base.py
@@ -158,9 +158,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},  # noqa
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},  # noqa
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"
-    },  # noqa
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},  # noqa
 ]
 
 # ------------------------------------------------------------------------------
