105c8667b98685850e2d3965ee1b3df89c638ae0
Tmuat <Thomas_Muat@hotmail.com>
2021-04-20 08:44:29 +0000

    PRODUCTS: Added more info to the custom model admin for categories

--- products/admin.py
+++ products/admin.py
@@ -5,13 +5,24 @@ from products.models import Category
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
-        'friendly_name',
-        'name',
-        'created',
-        'created_by',
-        'updated',
-        'updated_by'
+        "friendly_name",
+        "name",
+        "created",
+        "created_by",
+        "updated",
+        "updated_by",
     )
+    list_filter = ("created_by",)
+    search_fields = (
+        "name",
+        "created_by",
+    )
+    readonly_fields = [
+        "updated_by",
+        "updated",
+        "created_by",
+        "created",
+    ]
 
     def save_model(self, request, obj, form, change):
         if not obj.created_by:
