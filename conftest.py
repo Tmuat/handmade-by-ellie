d542975d5abde8b72f0e1171a131095b47e521bb
Tmuat <Thomas_Muat@hotmail.com>
2021-04-20 14:18:38 +0000

    CONFTEST: Added product fixture

--- conftest.py
+++ conftest.py
@@ -1,10 +1,18 @@
 import pytest
 
-from products.models import Category
+from products.models import Category, Product
 
 
 @pytest.fixture
 def category() -> Category:
     return Category.objects.create(
         name="test_category", friendly_name="Test Category"
+    )
+
+
+@pytest.fixture
+def product(category) -> Product:
+    return Product.objects.create(
+        sku=25545, name="Test Product", category=category,
+        description="Test description", price=10, slug="test-product"
     )
