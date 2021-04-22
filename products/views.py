760e33704f5bcc76df310bd9d79638059bcc41c5
Tmuat <Thomas_Muat@hotmail.com>
2021-04-22 12:41:38 +0000

    PRODUCTS: Added category searching

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 
-from products.models import Product
+from products.models import Product, Category
 
 
 def all_products(request):
@@ -13,8 +13,14 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if "category" in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if "query" in request.GET:
             query = request.GET["query"]
             if not query:
@@ -25,12 +31,15 @@ def all_products(request):
 
             queries = Q(name__icontains=query) | Q(
                 description__icontains=query
+            ) | Q(
+                category__friendly_name__icontains=query
             )
             products = products.filter(queries)
 
     context = {
         "products": products,
         "search_term": query,
+        "current_categories": categories,
     }
 
     return render(request, "products/products.html", context)
