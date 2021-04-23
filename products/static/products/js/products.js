3b3fa59a366acff1c27cc90ae3dd4003b71809cd
Tmuat <Thomas_Muat@hotmail.com>
2021-04-23 10:34:52 +0000

    PRODUCTS: Added side filter to all products

--- products/static/products/js/products.js
+++ products/static/products/js/products.js
@@ -18,3 +18,58 @@ $(window).resize(function () {
         $('.sidebar').removeAttr('style');
     }
 });
+
+
+// Code taken from Boutique Ado
+
+$('#sort-selector').change(function() {
+    var selector = $(this);
+    var currentUrl = new URL(window.location);
+
+    var selectedVal = selector.val();
+    if(selectedVal != "reset"){
+        var sort = selectedVal.split("_")[0];
+        var direction = selectedVal.split("_")[1];
+
+        currentUrl.searchParams.set("sort", sort);
+        currentUrl.searchParams.set("direction", direction);
+
+        window.location.replace(currentUrl);
+    } else {
+        currentUrl.searchParams.delete("sort");
+        currentUrl.searchParams.delete("direction");
+
+        window.location.replace(currentUrl);
+    }
+})
+
+// Code for the side filter
+
+$("button").click(function(){
+    var currentUrl = new URL(window.location);
+    var category = [];
+    var selectedVal = "";
+
+    $.each($("input[name='category_select']:checked"), function(){
+        category.push($(this).val());
+    });
+
+    $.each($("input[name='sort_radio']:checked"), function(){
+        selectedVal = selectedVal.replace("", $(this).val());
+    });
+
+    if (category.length > 0) {
+        var category_string = category.join(",")
+        currentUrl.searchParams.set("category", category_string);
+    }
+
+    if (selectedVal != "") {
+        var sort = selectedVal.split("_")[0];
+        var direction = selectedVal.split("_")[1];
+
+        currentUrl.searchParams.set("sort", sort);
+        currentUrl.searchParams.set("direction", direction);
+    }
+    
+    window.location.replace(currentUrl);
+});
