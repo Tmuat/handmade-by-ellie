9a18bc42736ca5e03d693e6c3dbafa828135aa3f
Tmuat <Thomas_Muat@hotmail.com>
2021-04-23 13:23:14 +0000

    BAG: Added test for bag template

--- bag/tests.py
+++ bag/tests.py
@@ -8,3 +8,7 @@ pytestmark = pytest.mark.django_db
 
 # View testing
 
+
+def test_bag_view_uses_correct_template(client):
+    response = client.get("/bag/")
+    assertTemplateUsed(response, "bag/bag.html")
