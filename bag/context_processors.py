8904596507fcaaae2688c6af1687e3e4002bb78c
Tmuat <Thomas_Muat@hotmail.com>
2021-04-26 16:11:31 +0000

    BAG: Added discount code to context processors

--- bag/context_processors.py
+++ bag/context_processors.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 
 from django.shortcuts import get_object_or_404
 
-from bag.models import DeliveryOptions
+from bag.models import DeliveryOptions, DiscountCode
 from products.models import Product
 
 
@@ -10,6 +10,7 @@ def bag_contents(request):
 
     bag_items = []
     delivery_option = None
+    discount_code = None
 
     total = 0
     delivery_cost = 0
@@ -18,6 +19,7 @@ def bag_contents(request):
 
     bag = request.session.get("bag", {})
     delivery_array = request.session.get("delivery", {})
+    discount = request.session.get("discount", {})
 
     for product_sku, quantity in bag.items():
         product = get_object_or_404(Product, sku=product_sku)
@@ -34,11 +36,22 @@ def bag_contents(request):
     delivery_sku = delivery_array.get("option")
     if delivery_sku is not None:
         delivery = get_object_or_404(DeliveryOptions, sku=delivery_sku)
-        delivery_cost = Decimal(delivery.price)
+        delivery_cost = delivery.price
         delivery_option = delivery
         delivery_set = True
     else:
         delivery_set = False
+
+    discount_code = discount.get("discount")
+    if discount_code is not None:
+        discount = get_object_or_404(DiscountCode, sku=discount_code)
+        discount_amount = 1 - Decimal(float(discount.discount))/100
+        total = total*discount_amount
+        discount_code = discount
+        discount_set = True
+
+    else:
+        discount_set = False
 
     grand_total = total + delivery_cost
 
@@ -49,6 +62,8 @@ def bag_contents(request):
         "grand_total": grand_total,
         "delivery_option": delivery_option,
         "delivery_set": delivery_set,
+        "discount_set": discount_set,
+        "discount_code": discount_code
     }
 
     return context
