62aa63720223bb114b3d139aa8cb4ed6e1d6b57d
Tmuat <Thomas_Muat@hotmail.com>
2021-04-26 10:58:31 +0000

    BAG: Added model admin

--- bag/admin.py
+++ bag/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from bag.models import DeliveryOptions
+
+
+class CustomDeliveryOptionsAdmin(admin.ModelAdmin):
+    list_display = ('option',
+                    'sku',
+                    'price',
+                    'description',
+                    'active',
+                    )
+    list_filter = ('option', 'price', 'description', 'active',)
+    search_fields = ('option',)
+    exclude = [
+        "updated_by",
+        "updated",
+        "created_by",
+        "created",
+    ]
+
+
+admin.site.register(DeliveryOptions, CustomDeliveryOptionsAdmin)
