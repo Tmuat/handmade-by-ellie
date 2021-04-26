f44cc5deb0b4a9de47a2ff282806bfa78c374024
Tmuat <Thomas_Muat@hotmail.com>
2021-04-26 10:59:58 +0000

    BAG: Added model admin

--- bag/admin.py
+++ bag/admin.py
@@ -10,6 +10,10 @@ class CustomDeliveryOptionsAdmin(admin.ModelAdmin):
                     'price',
                     'description',
                     'active',
+                    "created",
+                    "created_by",
+                    "updated",
+                    "updated_by",
                     )
     list_filter = ('option', 'price', 'description', 'active',)
     search_fields = ('option',)
@@ -19,6 +23,12 @@ class CustomDeliveryOptionsAdmin(admin.ModelAdmin):
         "created_by",
         "created",
     ]
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user.email
+        obj.updated_by = request.user.email
+        obj.save()
 
 
 admin.site.register(DeliveryOptions, CustomDeliveryOptionsAdmin)
