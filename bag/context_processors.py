9e351618a6468703439b8be0beecc73065a77ce0
Tmuat <Thomas_Muat@hotmail.com>
2021-04-27 13:24:31 +0000

    BAG: Added checks for discount codes being valid and product in stock

--- bag/context_processors.py
+++ bag/context_processors.py
@@ -1,9 +1,11 @@
 from decimal import Decimal
 
+from django.contrib import messages
 from django.shortcuts import get_object_or_404
+from django.utils import timezone
 
 from bag.models import DeliveryOptions, DiscountCode
-from products.models import Product
+from products.models import Product, ProductStock
 
 
 def bag_contents(request):
@@ -16,6 +18,9 @@ def bag_contents(request):
     delivery_cost = 0
     grand_total = 0
     product_count = 0
+    products_in_stock = True
+    discount_valid = False
+    discount_set = False
 
     bag = request.session.get("bag", {})
     delivery_array = request.session.get("delivery", {})
@@ -32,6 +37,9 @@ def bag_contents(request):
                 "product": product,
             }
         )
+        product_stock = ProductStock.objects.get(product=product)
+        if quantity > product_stock.available_stock:
+            products_in_stock = False
 
     delivery_sku = delivery_array.get("option")
     if delivery_sku is not None:
@@ -44,26 +52,66 @@ def bag_contents(request):
 
     discount_code = discount.get("discount")
     if discount_code is not None:
+        print("I GOT TO HERE")
         discount = get_object_or_404(DiscountCode, sku=discount_code)
-        discount_amount = 1 - Decimal(float(discount.discount)) / 100
-        total = total * discount_amount
-        discount_code = discount
-        discount_set = True
-
-    else:
-        discount_set = False
+        if discount.active:
+            print("I GOT TO HERE AND HERE")
+            if discount.set_expiry:
+                print("LOOKING AT EXPIRY")
+                if timezone.now() < discount.expiry:
+                    print("SEEMS OKAY")
+                    if discount.set_quantity:
+                        if discount.quantity > 0:
+                            discount_amount = 1 - Decimal(float(discount.discount)) / 100
+                            total = total * discount_amount
+                            discount_code = discount
+                            discount_valid = True
+                            discount_set = True
+                        else:
+                            messages.error(
+                                request, f"'{discount.code}' has no valid uses left. It has been removed."
+                            )
+                            del request.session["discount"]
+                    else:
+                        discount_amount = 1 - Decimal(float(discount.discount)) / 100
+                        total = total * discount_amount
+                        discount_code = discount
+                        discount_valid = True
+                        discount_set = True
+                else:
+                    messages.error(request, f"'{discount.code}' has expired.  It has been removed.")
+                    del request.session["discount"]
+            elif discount.set_quantity:
+                if discount.quantity > 0:
+                    discount_amount = 1 - Decimal(float(discount.discount)) / 100
+                    total = total * discount_amount
+                    discount_code = discount
+                    discount_valid = True
+                    discount_set = True
+                else:
+                    messages.error(
+                        request, f"'{discount.code}' has no valid uses left.  It has been removed."
+                    )
+                    del request.session["discount"]
+        else:
+            messages.error(
+                request, f"'{discount.code}' is not an active discount code.  It has been removed."
+            )
+            del request.session["discount"]
 
     grand_total = total + delivery_cost
 
     context = {
         "bag_items": bag_items,
         "product_count": product_count,
+        "product_stock": products_in_stock,
         "total": total,
         "grand_total": grand_total,
         "delivery_option": delivery_option,
         "delivery_set": delivery_set,
         "discount_set": discount_set,
         "discount_code": discount_code,
+        "discount_valid": discount_valid
     }
 
     return context
