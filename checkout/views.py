d49fec5c89efe95253275c9ec49380516f0c595a
Tmuat <Thomas_Muat@hotmail.com>
2021-04-27 10:23:49 +0000

    CHECKOUT: Created checkout view & url

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect, reverse
 
-# Create your views here.
+from checkout.forms import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    delivery_option = request.session.get('delivery', {})
+    discount = request.session.get("discount", {})
+
+    if not bag:
+        messages.error(request,
+                       "There is nothing in your bag at the moment")
+        return redirect(reverse('bag'))
+
+    if not delivery_option:
+        messages.error(request,
+                       "There is no delivery option selected")
+        return redirect(reverse('bag'))
+
+    order_form = OrderForm()
+
+    template = 'checkout/checkout.html'
+
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
