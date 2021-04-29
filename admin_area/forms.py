b0b0d5d5d1f54d974232585c4fd7860f8edec297
Tmuat <Thomas_Muat@hotmail.com>
2021-04-29 08:33:52 +0000

    ADMIN: Created discount form and formset

--- admin_area/forms.py
+++ admin_area/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 
-
-from bag.models import DeliveryOptions
+from bag.models import DeliveryOptions, DiscountCode
 
 
 class DeliveryForm(forms.ModelForm):
@@ -41,4 +40,28 @@ class DeliveryForm(forms.ModelForm):
 DeliveryFormset = forms.modelformset_factory(
     DeliveryOptions,
     form=DeliveryForm,
+)
+
+
+class DiscountForm(forms.ModelForm):
+    class Meta:
+        model = DiscountCode
+        fields = "code", "discount", "active", "expiry", "quantity"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name == 'active':
+                field.widget.attrs[
+                    "class"
+                ] = "border-0 rounded-0 w-100 h-100 no-active bg-transparent"
+            else:
+                field.widget.attrs[
+                    "class"
+                ] = "border-0 rounded-0 w-100 h-100 no-active"
+
+
+DiscountFormset = forms.modelformset_factory(
+    DiscountCode,
+    form=DiscountForm,
 )
