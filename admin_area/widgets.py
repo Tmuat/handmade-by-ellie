c93824e1d826bab67fb507f1d11c6f49e962048d
Tmuat <Thomas_Muat@hotmail.com>
2021-04-29 09:49:37 +0000

    ADMIN: Created custom widget

---
+++ admin_area/widgets.py
@@ -0,0 +1,8 @@
+from django.forms import widgets
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomClearableFileInputImages(widgets.ClearableFileInput):
+    clear_checkbox_label = _('Change')
+    input_text = _('')
+    template_name = 'admin_area/custom_widget_templates/custom_clearable_file_input_images.html'
